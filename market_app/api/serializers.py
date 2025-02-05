dfdef4157e18f1d6553d904cf04254192cc4936a
Anna Fritz <anna_eida@gmx.de>
2025-02-05 12:46:21 +0100

    added validate function for location to serializer

--- market_app/api/serializers.py
+++ market_app/api/serializers.py
@@ -19,3 +19,8 @@ class MarketSerializer(serializers.Serializer):
         instance.net_worth = validated_data.get('net_worth', instance.net_worth)
         instance.save()
         return instance
+
+    def validate_location(self, value):
+        if 'X' in value:
+            raise serializers.ValidationError('Please No X in location')
+        return value

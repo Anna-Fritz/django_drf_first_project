1e2587f92157d3856d9629016a0b5e216d3a9ec3
Anna Fritz <anna_eida@gmx.de>
2025-02-07 23:32:07 +0100

    added method to modify fields dynamically as args of Serialier

--- market_app/api/serializers.py
+++ market_app/api/serializers.py
@@ -64,9 +64,31 @@ class MarketSerializer(serializers.ModelSerializer):
 class MarketHyperlinkedSerializer(MarketSerializer, serializers.HyperlinkedModelSerializer):
     sellers = None
 
+    """
+    modifying fields dynamically
+    A ModelSerializer that takes an additional `fields` argument that
+    controls which fields should be displayed.
+    displayed fields have to be defined in markets_view while creating MarketHyperlinkedSerializer
+    """
+
+    # def __init__(self, *args, **kwargs):
+    #     # Don't pass the 'fields' arg up to the superclass
+    #     fields = kwargs.pop('fields', None)
+
+    #     # Instantiate the superclass normally
+    #     super().__init__(*args, **kwargs)
+
+    #     if fields is not None:
+    #         # Drop any fields that are not specified in the `fields` argument.
+    #         allowed = set(fields)
+    #         existing = set(self.fields)
+    #         for field_name in existing - allowed:
+    #             self.fields.pop(field_name)
+
     class Meta:
         model = Market
         fields = ['id', 'name', 'url', 'location', 'description', 'net_worth']
+
 
 
 
